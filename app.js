e0c02720c86f2a734fb1572eb6f6f7cb3fc83359
Shad <iamshadmirza@gmail.com>
2019-07-26 08:22:39 +0530

    connection to local database

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const connectionUrl = 'mongodb://127.0.0.1:27017';
+const databaseName = 'book-store';
+
+mongoose.connect(connectionUrl + '/' + databaseName, { useNewUrlParser: true }, (error, client) => {
+    console.log({ error, client });
+    if (error) {
+        return console.log('Unable to connect to database');
+    }
+    console.log('connected to mongodb database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
